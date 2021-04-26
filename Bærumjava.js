b6888b13d839f028b9d535fcfb7fc8cc95aa5e75
Embril <77843171+Embril@users.noreply.github.com>
2021-04-26 10:38:08 +0100

    Add files via upload

---
+++ Bærumjava.js
@@ -0,0 +1,12 @@
+var navside = document.getElementById('mySidenav');
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  navside.style.width = '250px';
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  navside.style.width = '0';
+}
